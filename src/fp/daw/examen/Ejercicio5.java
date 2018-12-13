3e0978385e9f8dafc5b1c3c6a34574f6e9b58dd8
Olmo <olmot.s@gmail.com>
2018-12-13 13:23:34 +0100

    Eejrcicio 5 RESUELTO

--- src/fp/daw/examen/Ejercicio5.java
+++ src/fp/daw/examen/Ejercicio5.java
@@ -1,5 +1,8 @@
 package fp.daw.examen;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	/* 
@@ -16,8 +19,28 @@ public class Ejercicio5 {
 	 */
 	
 	public static void main(String[] args) {
+		Random r = new Random();
+		Scanner teclado = new Scanner(System.in);
+		int respuesta = 0;
+		int retry = 1;
+		int intentos = 0;
+		int numero = r.nextInt (((20-10)+1)+10 ); 
+		System.out.println("Adivina el numero aleatorio entre 100 y 200");
+		long Marty = System.currentTimeMillis();
+		do {
 		
-
+		do {
+			respuesta = teclado.nextInt();
+			System.out.print("El numero no es correcto, lo siento amigo, intentalo de nuevo ;)");
+			intentos++;
+		}while(respuesta != numero);
+		 long Doc = System.currentTimeMillis();
+		System.out.println("¡Correcto!, el numero era: " + numero);
+		System.out.println("Nº De Intentos: " + intentos);
+		System.out.println("Has tardado: " + (Doc-Marty) + " Milisegundos");
+		System.out.println("¿Quiéres intentanrlo otra vez? SI(1)/NO(0)");
+		retry = teclado.nextInt();
+		}while(retry ==1);
 	}
 
 }
